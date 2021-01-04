71bfc554f63216dfe62a68b5e8dedf810483cd06
shrutisrisetty <76504786+shrutisrisetty@users.noreply.github.com>
2021-01-04 19:14:00 +0800

    Update QR_web.py

--- QR_web.py
+++ QR_web.py
@@ -5,7 +5,7 @@ from pyqrcode import QRCode
 
 
 # String which represents the QR code 
-s = "https://shrutisrisetty.github.io/educational-website-/"
+s = "https://shrutisrisetty.github.io/educational-website-mobile-/"
 # Generate QR code 
 url = pyqrcode.create(s) 
 
